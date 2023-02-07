836b191c93e46753667a79aa70436821ecdf6250
AlbertoDev1 <josealbertoj584@gmail.com>
2023-02-07 20:49:26 -0300

    Versão 0.9

---
+++ media.py
@@ -0,0 +1,17 @@
+# Calculate the average of 4 numbers
+
+def average(num1, num2, num3, num4):
+  # Calculate the sum
+  sum = num1 + num2 + num3 + num4
+
+  # Calculate the average
+  avg = sum / 5
+
+  # Return the average
+  return avg
+
+# Call the function with four numbers
+result = average(1, 2, 3, 4, 5)
+
+# Print the result
+print(result) 
